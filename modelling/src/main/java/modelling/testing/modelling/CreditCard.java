49b10968fb219f77142f15ff04945590d4a20fca
yonghan <wangyh.92@gmail.com>
2021-02-23 22:04:53 +0800

    change variable types and passes 4 test

--- modelling/src/main/java/modelling/testing/modelling/CreditCard.java
+++ modelling/src/main/java/modelling/testing/modelling/CreditCard.java
@@ -1,5 +1,6 @@
 package modelling.testing.modelling;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 import org.javamoney.moneta.Money;
@@ -8,31 +9,40 @@ public class CreditCard {
 	
 	UUID id = UUID.randomUUID();
 	
-	private Money balance;
+	private BigDecimal balance;
+	private String currency;
 	private int withdrawals;
 	
 	public CreditCard(Money balance) {
-		this.balance = balance;
+		this.balance = balance.getNumberStripped();
+		this.currency = balance.getCurrency().getCurrencyCode();
 	}
 
 	Result withdraw(Money withdrawalAmount) {
 		if(notEnoughMoneyToWithdraw(withdrawalAmount)) return Result.Failure;
 		
-		balance = balance.subtract(withdrawalAmount);
+		if (tooManyWithdrawalsInCycle()) return Result.Failure;
+		
+		balance = balance.subtract(withdrawalAmount.getNumberStripped());
+		withdrawals++;
 		return Result.Success;
 	}
 	
+	private boolean tooManyWithdrawalsInCycle() {
+		return withdrawals >= 45;
+	}
+	
 	private boolean notEnoughMoneyToWithdraw(Money withdrawalAmount) {
-		return withdrawalAmount.isGreaterThan(balance);
+		return withdrawalAmount.getNumberStripped().compareTo(balance) > 0;
 	}
 	
 	Result repay(Money repaymentAmount) {
-		balance = balance.add(repaymentAmount);
+		balance = balance.add(repaymentAmount.getNumberStripped());
 		return Result.Success;
 	}
 	
 	Money avaliableLimit(){
-		return balance;
+		return Money.of(balance, currency);
 	}
 	
 }
